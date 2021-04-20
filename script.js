9ebd7288705f66a35d26f7a3f4e28b8b1a0abd33
Benny Van Hoof <benny.vanhoof@be.ibm.com>
2021-04-20 17:56:41 +0200

    first update

--- script.js
+++ script.js
@@ -1,6 +1,34 @@
+
+
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    /* This function calculates the interest to receive 
+    input : the number of years, the rate and the ptinciple
+    output: the interest to receive */
+    var years = document.getElementById('years').value
+    var year = new Date().getFullYear() + parseInt(years) 
+    var rate = document.getElementById("rate").value;
+    var principal = document.getElementById("principal").value;
+    var interest = principal * years * rate / 100
+    document.getElementById("result").innerHTML='If you deposit '+principal+',\
+    <br\>at an interest rate of '+rate+'%' + '\
+    <br\>You will receive an interest amount of '+ interest + '\
+    in the year '+year+'\<br\>'      
 }
+
+
+function updateRate() 
+{
+    /* This function adjust the interest % , according to the movement
+    of the slidebar */
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval+ ' %';
+}
+
+
+
+
+
+
+
         
